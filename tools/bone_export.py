3bb5d03fe8e8bcf75bc86a24d0456b7679ec2c56
Ingo Ruhnke <grumbel@gmail.com>
2007-06-20 21:56:14 +0000

    - some work on the bone exporter

--- tools/bone_export.py
+++ tools/bone_export.py
@@ -1,73 +1,78 @@
-import os
 import Blender
 import Blender.Armature
 
-os.system('clear')
 arms = Blender.Armature.Get()
-print ";;;;;;;;;;;;; Start of File ;;;;;;;;;;;;;;;;"
 
 def vec2str(vec):
-	return "%f %f %f" % (vec.x,vec.y,vec.z)
+    return "%f %f %f" % (vec.x,vec.y,vec.z)
+
+def euler2str(euler):
+    return "%f %f %f" % (euler.x, euler.y, euler.z)
 
 def quat2str(quat):
-	return "%f %f %f %f" % (quat.x, quat.y, quat.z, quat.w)
+    return "%f %f %f %f" % (quat.x, quat.y, quat.z, quat.w)
 
 def matrix2str(indent, m):
-	return          ("%9f %9f %9f\n" % (m[0][0], m[0][1], m[0][2])) + \
+        return          ("%9f %9f %9f\n" % (m[0][0], m[0][1], m[0][2])) + \
            indent + ("%9f %9f %9f\n" % (m[1][0], m[1][1], m[1][2])) + \
            indent + ("%9f %9f %9f" % (m[2][0], m[2][1], m[2][2]))
 
 def list2str(lst):
-	str = ""
-	for i in lst:
-		str += " \"%s\"" % i
-	return str
+    str = ""
+    for i in lst:
+        str += " \"%s\"" % i
+    return str
 
-for arm in arms.values():	
-	print "(armature"
-	print "  (name \"%s\")" % (arm.name,)
-	print "  (bones "
-	
-	for bone in arm.bones.values():
-		if bone.name[:3] == "IK_": # ignore helper bones
-			print "    ;; ignoring bone: %s" % bone.name
-			print
-		else:
-			if bone.parent:
-				print "    (bone"
-			else:
-				print "    (bone ;; Parentless bone, aka Master bone XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-				
-			print "      (name      \"%s\")" % bone.name
-			
-			print "      (children %s)" % list2str(map(lambda b: b.name, bone.children))
-							
-			# If a bone lacks parent its head is in armature space,
-			# else in local bonespace
-			if bone.parent:
-				print "      (parent    \"%s\")" % bone.parent.name
-				print "      (head      %s)" % (vec2str(bone.head['BONESPACE']),)
-			else:
-				print "      (parent )"
-				print "      (head      %s)" % (vec2str(bone.head['ARMATURESPACE']),)				
-			
-			print "      (length    %s)" % bone.length
-			# print "      (matrix   %s)" % matrix2str(" "*16, bone.matrix['BONESPACE'])
-			print "      (quat      %s)" % quat2str(bone.matrix['BONESPACE'].toQuat())
-			# print "###  BoneSpace:"
-			# print bone.matrix['BONESPACE']
-			# print
-			# print "  ArmatureSpace:"
-			#  print bone.matrix['ARMATURESPACE']
-			print "     )\n"
+def export_armature(out, armature):
+    out.write(";; -*- scheme -*-\n")
+    out.write("(armature\n")
+    out.write("  (name \"%s\")\n" % armature.name)
+    out.write("  (bones\n")
 
-	print "  ) ;; bones"
-print " ) ;; armature"
+    for bone in armature.bones.values():
+        if bone.name[:3] == "IK_": # ignore helper bones
+            out.write("    ;; ignoring bone: %s\n" % bone.name)
+            out.write("\n")
+        else:
+            if bone.parent:
+                out.write("    (bone\n")
+            else:
+                out.write("    (bone ;; a root bone\n")
 
-# mesh = Blender.NMesh.GetRaw("Cube.001")
-# print mesh.faces[0].v[0].index
+            out.write("      (name      \"%s\")\n" % bone.name)
 
-# for v in mesh.verts:
-#	mesh.getVertexInfluences(v.index)
-	
+            out.write("      (children %s)\n" % list2str(map(lambda b: b.name, bone.children)))
+
+            # If a bone lacks parent its head is in armature space,
+            # else in local bonespace
+            if bone.parent:
+                out.write("      (parent    \"%s\")\n" % bone.parent.name)
+                out.write("      (head      %s)\n" % (vec2str(bone.head['BONESPACE']),))
+            else:
+                out.write("      (parent )\n")
+                out.write("      (head      %s)\n" % (vec2str(bone.head['ARMATURESPACE']),))
+
+            out.write("      (length    %s)\n" % bone.length)
+            out.write("      (matrix   %s)\n" % matrix2str(" "*16, bone.matrix['BONESPACE']))
+            out.write("      (quat      %s)\n" % quat2str(bone.matrix['BONESPACE'].toQuat()))
+            out.write("      (euler     %s)\n" % euler2str(bone.matrix['BONESPACE'].toEuler()))
+            # out.write("###  BoneSpace:"
+            # out.write(bone.matrix['BONESPACE']
+            # print
+            # out.write("  ArmatureSpace:"
+            #  out.write(bone.matrix['ARMATURESPACE']
+            out.write("     )\n\n")
+    out.write("  ) ;; bones\n")
+    out.write(" ) ;; armature\n")
+
+print "Windstille Bone Exporter:"
+for armature in arms.values():
+    filename = "/tmp/bone-%s.scm" % armature.name
+    out = file(filename, "w")
+    print "  - exporting to '%s'..." % filename,
+    export_armature(out, armature)
+    out.close()
+    print "done"
+print
+
 # EOF #
