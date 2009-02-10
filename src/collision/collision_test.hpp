b9ef1b67b633aaf399df0f7162ab93cc4ee0b6e7
Ingo Ruhnke <grumbel@gmail.com>
2009-02-10 14:35:30 +0000

    Switched to GPLv3, cleaned up copyright header

--- src/collision/collision_test.hpp
+++ src/collision/collision_test.hpp
@@ -1,23 +1,21 @@
 /*
- * Copyright (c) 2005 by the Windstille team. All rights reserved.
- *
- * collision_test.hxx
- * by David Kamphausen (david.kamphausen@web.de)
- *    Ingo Ruhnke
- *
- * The "Windstille" project, including all files needed to compile it,
- * is free software; you can redistribute it and/or use it and/or modify it
- * under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.
- */
+**  Windstille - A Sci-Fi Action-Adventure Game
+**  Copyright (C) 2005 David Kamphausen <david.kamphausen@web.de>,
+**                     Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**  
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**  
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef HEADER_COLLTEST_HXX
 #define HEADER_COLLTEST_HXX
