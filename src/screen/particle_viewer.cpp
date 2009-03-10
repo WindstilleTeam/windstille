5b2d52b7832269824878e7ee9602afe5db9bf865
Ingo Ruhnke <grumbel@gmail.com>
2009-03-10 01:24:03 +0000

    Fill whole screen on non 800x600 resolutions

--- src/screen/particle_viewer.cpp
+++ src/screen/particle_viewer.cpp
@@ -22,6 +22,7 @@
 #include "screen/particle_viewer.hpp"
 #include "screen_manager.hpp"
 #include "util/sexpr_file_reader.hpp"
+#include "display/display.hpp"
 
 // Components
 #include "gui/button.hpp"
@@ -146,10 +147,18 @@ ParticleViewer::draw()
 {
   sc.reset_modelview();
 
-  for(int y = -background.get_width(); y < 600 + background.get_height(); y += background.get_height())
-    for(int x = -background.get_width(); x < 800 + background.get_width(); x += background.get_width())
-      sc.color().draw(background, Vector2f(x + int(pos.x) % background.get_width(),
-                                         y + int(pos.y) % background.get_height()), -900);
+  for(int y = -background.get_width(); 
+      y < Display::get_height() + background.get_height(); 
+      y += background.get_height())
+    {
+      for(int x = -background.get_width(); 
+          x < Display::get_width() + background.get_width(); 
+          x += background.get_width())
+        {
+          sc.color().draw(background, Vector2f(x + int(pos.x) % background.get_width(),
+                                               y + int(pos.y) % background.get_height()), -900);
+        }
+    }
 
   sc.translate(400 + pos.x, 600 + pos.y);
   sc.light().fill_screen(Color(0.4, 0.4, 0.4));
