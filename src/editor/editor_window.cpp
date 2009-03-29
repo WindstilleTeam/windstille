6d6d8377c7c7921afdb1c2576a631a3e426bac6a
Ingo Ruhnke <grumbel@gmail.com>
2009-03-29 05:09:32 +0000

    Some new graphics and a new test sector

--- src/editor/editor_window.cpp
+++ src/editor/editor_window.cpp
@@ -337,6 +337,7 @@ EditorWindow::EditorWindow(const Glib::RefPtr<const Gdk::GL::Config>& glconfig_)
   add(vbox);
 
   fill_object_selector("data/images/decal/");
+  fill_object_selector("data/images/objects/bar/");
   //fill_object_selector("data/images/inventory/");
   //fill_object_selector("data/images/portraits/");
   fill_object_selector("data/images/objects/");
