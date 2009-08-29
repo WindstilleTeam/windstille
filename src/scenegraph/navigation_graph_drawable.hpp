b49a6ec304ea9d921760273a683ba1eb03236620
Ingo Ruhnke <grumbel@gmail.com>
2009-08-29 10:52:53 +0000

    Fixed include guard

--- src/scenegraph/navigation_graph_drawable.hpp
+++ src/scenegraph/navigation_graph_drawable.hpp
@@ -16,8 +16,8 @@
 **  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef HEADER_NAVIGATION_GRAPH_DRAWABLE_HPP
-#define HEADER_NAVIGATION_GRAPH_DRAWABLE_HPP
+#ifndef HEADER_WINDSTILLE_SCENEGRAPH_NAVIGATION_GRAPH_DRAWABLE_HPP
+#define HEADER_WINDSTILLE_SCENEGRAPH_NAVIGATION_GRAPH_DRAWABLE_HPP
 
 #include "display/display.hpp"
 #include "scenegraph/drawable.hpp"
