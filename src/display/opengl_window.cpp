b582abacdb3994f69d16d8c33271a03fffa837de
Ingo Ruhnke <grumbel@gmail.com>
2009-08-30 23:08:20 +0000

    Check if OpenGL supports FramebufferObject

--- src/display/opengl_window.cpp
+++ src/display/opengl_window.cpp
@@ -54,19 +54,19 @@ OpenGLWindow::OpenGLWindow()
   else
     {
       GLenum err = glewInit();
-      if(err != GLEW_OK) 
+      if (err != GLEW_OK) 
       {
         std::ostringstream msg;
         msg << "Display:: Couldn't initialize glew: " << glewGetString(err);
         throw std::runtime_error(msg.str());
       }
-      /*
-        if(!GLEW_EXT_framebuffer_object) {
+
+      if (!GLEW_EXT_framebuffer_object) 
+      {
         std::ostringstream msg;
         msg << "Display:: Framebuffer opengl extension not supported";
         throw std::runtime_error(msg.str());
-        }
-      */
+      }
 
       glViewport(0, 0, m_window->w, m_window->h);
       glMatrixMode(GL_PROJECTION);
@@ -74,9 +74,6 @@ OpenGLWindow::OpenGLWindow()
 
       static const float cl_pixelcenter_constant = 0.375;
 
-      //glOrtho(0.0, window->w, window->h, 0.0, -1000.0, 1000.0);
-  
-      // glOrtho(0.0, 800, 0.0, 600.0, 1000.0, -1000.0); // proper right-hand CO
       Display::aspect_size = Size(config.get_int("aspect-width"), 
                                   config.get_int("aspect-height"));
 
