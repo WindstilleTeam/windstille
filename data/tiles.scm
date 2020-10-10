fd48df5acb0a0a196902de4a130fd758acee2584
Ingo Ruhnke <grumbel@gmail.com>
2020-10-10 17:21:52 +0200

    Adjust data/ files to reflect FileReader changes

--- data/tiles.scm
+++ data/tiles.scm
@@ -1,4 +1,5 @@
 (windstille-tiles
+ (tiles
  (tilegroup (name "Virtual Reality")
             (tiles
              1 2 3 4 5 6 0 0
@@ -497,6 +498,6 @@
          0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
          0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0        
          ))
- )
+ ))
 
 ;; EOF ;;
