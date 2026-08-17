130fca1d2c5d7a152dc1c33d13d76b0c5ab4b29e
Ingo Ruhnke <grumbel@gmail.com>
2026-08-17 17:55:25 +0000

    Remove flake dependencies that are now available as external/ subtrees
    The libraries that used to be pulled in as separate flake inputs
    (argpp, babyxml, biiocpp/geomcpp, logmich, priocpp, surfcpp, the wst*
    modules, miniswig, ...) live under external/ as git subtrees.  The
    build already falls back to those directories via
    tinycmmc_find_dependency(), so the corresponding inputs and the
    explicit package references in buildInputs can be dropped.
    
    tinycmmc is kept only because the remaining squirrel input still
    depends on it; the library helpers used by this flake itself are
    taken from the local external/tinycmmc copy.  squirrel and the
    Windows-specific SDL2-win32 stay as remote inputs because they have
    no corresponding subtree.
    
    Co-authored-by: Grok <grok@x.ai>

--- flake.nix
+++ flake.nix
@@ -1,106 +1,32 @@
-rec {
+{
   description = "Windstille GUI Engine";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
+    # Still required as a remote input because squirrel depends on it
+    # (via follows) and is not available as a subtree.
     tinycmmc.url = "git+https://github.com/grumbel/tinycmmc.git";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
     tinycmmc.inputs.flake-utils.follows = "flake-utils";
-
-    argpp.url = "git+https://github.com/grumbel/argpp.git?ref=stable";
-    argpp.inputs.nixpkgs.follows = "nixpkgs";
-    argpp.inputs.flake-utils.follows = "flake-utils";
-    argpp.inputs.tinycmmc.follows = "tinycmmc";
-
-    logmich.url = "git+https://github.com/logmich/logmich.git";
-    logmich.inputs.nixpkgs.follows = "nixpkgs";
-
-    geomcpp.url = "git+https://github.com/grumbel/geomcpp.git";
-    geomcpp.inputs.nixpkgs.follows = "nixpkgs";
-    geomcpp.inputs.tinycmmc.follows = "tinycmmc";
-
-    sexpcpp.url = "git+https://github.com/lispparser/sexp-cpp.git";
-    sexpcpp.inputs.nixpkgs.follows = "nixpkgs";
-    sexpcpp.inputs.flake-utils.follows = "flake-utils";
-
-    priocpp.url = "git+https://github.com/grumbel/priocpp.git";
-    priocpp.inputs.nixpkgs.follows = "nixpkgs";
-    priocpp.inputs.flake-utils.follows = "flake-utils";
-    priocpp.inputs.logmich.follows = "logmich";
-    priocpp.inputs.sexpcpp.follows = "sexpcpp";
-
-    surfcpp.url = "git+https://github.com/grumbel/surfcpp.git";
-    surfcpp.inputs.nixpkgs.follows = "nixpkgs";
-    surfcpp.inputs.tinycmmc.follows = "tinycmmc";
-    surfcpp.inputs.geomcpp.follows = "geomcpp";
-    surfcpp.inputs.logmich.follows = "logmich";
-    surfcpp.inputs.SDL2-win32.follows = "SDL2-win32";
-
-    babyxml.url = "git+https://github.com/grumbel/babyxml.git";
-    babyxml.inputs.nixpkgs.follows = "nixpkgs";
-    babyxml.inputs.tinycmmc.follows = "tinycmmc";
-
-    biiocpp.url = "git+https://github.com/grumbel/biiocpp.git";
-    biiocpp.inputs.nixpkgs.follows = "nixpkgs";
-    biiocpp.inputs.tinycmmc.follows = "tinycmmc";
-
-    wstinput.url = "git+https://github.com/WindstilleTeam/wstinput.git";
-    wstinput.inputs.nixpkgs.follows = "nixpkgs";
-    wstinput.inputs.flake-utils.follows = "flake-utils";
-    wstinput.inputs.tinycmmc.follows = "tinycmmc";
-    wstinput.inputs.logmich.follows = "logmich";
-    wstinput.inputs.priocpp.follows = "priocpp";
-    wstinput.inputs.SDL2-win32.follows = "SDL2-win32";
-    wstinput.inputs.sexpcpp.follows = "sexpcpp";
-
-    wstdisplay.url = "git+https://github.com/WindstilleTeam/wstdisplay.git";
-    wstdisplay.inputs.nixpkgs.follows = "nixpkgs";
-    wstdisplay.inputs.tinycmmc.follows = "tinycmmc";
-    wstdisplay.inputs.geomcpp.follows = "geomcpp";
-    wstdisplay.inputs.babyxml.follows = "babyxml";
-    wstdisplay.inputs.logmich.follows = "logmich";
-    wstdisplay.inputs.SDL2-win32.follows = "SDL2-win32";
-    wstdisplay.inputs.surfcpp.follows = "surfcpp";
-
-    wstgui.url = "git+https://github.com/WindstilleTeam/wstgui.git";
-    wstgui.inputs.nixpkgs.follows = "nixpkgs";
-    wstgui.inputs.flake-utils.follows = "flake-utils";
-    wstgui.inputs.tinycmmc.follows = "tinycmmc";
-    wstgui.inputs.babyxml.follows = "babyxml";
-    wstgui.inputs.geomcpp.follows = "geomcpp";
-    wstgui.inputs.logmich.follows = "logmich";
-    wstgui.inputs.priocpp.follows = "priocpp";
-    wstgui.inputs.surfcpp.follows = "surfcpp";
-    wstgui.inputs.wstinput.follows = "wstinput";
-    wstgui.inputs.wstdisplay.follows = "wstdisplay";
-    wstgui.inputs.wstsound.follows = "wstsound";
-    wstgui.inputs.sexpcpp.follows = "sexpcpp";
-
-    wstsound.url = "git+https://github.com/WindstilleTeam/wstsound.git";
-    wstsound.inputs.nixpkgs.follows = "nixpkgs";
-    wstsound.inputs.flake-utils.follows = "flake-utils";
-    wstsound.inputs.tinycmmc.follows = "tinycmmc";
 
     squirrel.url = "git+https://github.com/grumnix/squirrel.git";
     squirrel.inputs.nixpkgs.follows = "nixpkgs";
     squirrel.inputs.tinycmmc.follows = "tinycmmc";
 
-    miniswig.url = "git+https://github.com/WindstilleTeam/miniswig.git";
-    miniswig.inputs.nixpkgs.follows = "nixpkgs";
-    miniswig.inputs.squirrel.follows = "squirrel";
-    miniswig.inputs.tinycmmc.follows = "tinycmmc";
-
     SDL2-win32.url = "git+https://github.com/grumnix/SDL2-win32.git";
     SDL2-win32.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils,
-              tinycmmc, argpp, logmich, geomcpp, priocpp, surfcpp, babyxml, biiocpp, sexpcpp,
-              wstinput, wstdisplay, wstgui, wstsound, miniswig, squirrel, SDL2-win32 }:
-
-    tinycmmc.lib.eachSystemWithPkgs (pkgs:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc, squirrel, SDL2-win32 }:
+    let
+      # Prefer the local tinycmmc subtree for the library helpers used
+      # by this flake; the remote input is only kept to satisfy
+      # squirrel's dependency.
+      tinycmmc_lib = import ./external/tinycmmc { inherit nixpkgs flake-utils; };
+    in
+    tinycmmc_lib.eachSystemWithPkgs (pkgs:
       {
         packages = rec {
           default = windstille;
@@ -122,34 +48,35 @@ rec {
               pkgs.buildPackages.cmake
               pkgs.buildPackages.pkg-config
               pkgs.buildPackages.xcftools
-
               pkgs.buildPackages.bison
               pkgs.buildPackages.flex
-            ] ++ [
-              miniswig.packages.${pkgs.stdenv.buildPlatform.system}.default
             ] ++
             (nixpkgs.lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.makeWrapper);
 
             buildInputs = [
-              argpp.packages.${pkgs.stdenv.hostPlatform.system}.default
-              babyxml.packages.${pkgs.stdenv.hostPlatform.system}.default
-              biiocpp.packages.${pkgs.stdenv.hostPlatform.system}.default
-              geomcpp.packages.${pkgs.stdenv.hostPlatform.system}.default
-              logmich.packages.${pkgs.stdenv.hostPlatform.system}.default
-              priocpp.packages.${pkgs.stdenv.hostPlatform.system}.priocpp-sexp
-              surfcpp.packages.${pkgs.stdenv.hostPlatform.system}.default
-              tinycmmc.packages.${pkgs.stdenv.hostPlatform.system}.default
-              wstdisplay.packages.${pkgs.stdenv.hostPlatform.system}.default
-              wstgui.packages.${pkgs.stdenv.hostPlatform.system}.default
-              wstinput.packages.${pkgs.stdenv.hostPlatform.system}.default
-              wstsound.packages.${pkgs.stdenv.hostPlatform.system}.default
+              # The former flake-provided libraries (argpp, babyxml, ...,
+              # wstgui, ...) are now built from the external/ subtrees by
+              # tinycmmc_find_dependency().  Only their transitive
+              # dependencies that come from nixpkgs (or the remaining
+              # remote flakes) need to be listed here.
+              pkgs.fmt
+              pkgs.gtest
+              pkgs.glm
+              pkgs.jsoncpp
+              pkgs.libjpeg
+              pkgs.libpng
+              pkgs.glew
+              pkgs.libGL
+              pkgs.SDL2
+              pkgs.sigc++
 
               squirrel.packages.${pkgs.stdenv.hostPlatform.system}.default
-
-              pkgs.gtest
             ] ++
             (nixpkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
               pkgs.gtkmm3
+            ]) ++
+            (nixpkgs.lib.optionals pkgs.stdenv.hostPlatform.isWindows [
+              SDL2-win32.packages.${pkgs.stdenv.hostPlatform.system}.default
             ]);
           };
         };
