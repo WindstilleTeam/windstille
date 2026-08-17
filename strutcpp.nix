409f14fe30c9e6ac1633b337ca96846dadaf9c3d
Ingo Ruhnke <grumbel@gmail.com>
2026-08-17 18:05:56 +0000

    Squashed 'external/strutcpp/' content from commit 81bbd72
    git-subtree-dir: external/strutcpp
    git-subtree-split: 81bbd72b9a5ecc67dbcf18ffa9117cdf6f21262a

---
+++ strutcpp.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, lib
+, cmake
+, gtest
+}:
+
+stdenv.mkDerivation {
+  pname = "strutcpp";
+  version = "0.0.0";
+
+  src = lib.cleanSource ./.;
+
+  cmakeFlags = [
+    "-DBUILD_TESTS=ON"
+    "-DWARNINGS=ON"
+    "-DWERROR=ON"
+  ];
+
+  doCheck = true;
+
+  checkPhase = ''
+    make test ARGS="--verbose"
+  '';
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  buildInputs = [
+    gtest
+  ];
+}
